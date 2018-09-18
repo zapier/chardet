152ff3036da8aba7e3eee5a22c579a1cd4086819
Claudiu Popa <pcmanticore@gmail.com>
2018-09-18 11:12:30 +0200

    Make certain blobs binary

--- chardet/universaldetector.py
+++ chardet/universaldetector.py
@@ -69,19 +69,19 @@ class UniversalDetector:
 
         charmap = (
             # EF BB BF  UTF-8 with BOM
-            ('\xEF\xBB\xBF', {'encoding': "UTF-8", 'confidence': 1.0}),
+            (b'\xEF\xBB\xBF', {'encoding': "UTF-8", 'confidence': 1.0}),
             # FF FE 00 00  UTF-32, little-endian BOM
-            ('\xFF\xFE\x00\x00', {'encoding': "UTF-32LE", 'confidence': 1.0}),
+            (b'\xFF\xFE\x00\x00', {'encoding': "UTF-32LE", 'confidence': 1.0}),
             # 00 00 FE FF  UTF-32, big-endian BOM
-            ('\x00\x00\xFE\xFF', {'encoding': "UTF-32BE", 'confidence': 1.0}),
+            (b'\x00\x00\xFE\xFF', {'encoding': "UTF-32BE", 'confidence': 1.0}),
             # FE FF 00 00  UCS-4, unusual octet order BOM (3412)
-            (u'\xFE\xFF\x00\x00', {'encoding': "X-ISO-10646-UCS-4-3412", 'confidence': 1.0}),
+            (b'\xFE\xFF\x00\x00', {'encoding': "X-ISO-10646-UCS-4-3412", 'confidence': 1.0}),
             # 00 00 FF FE  UCS-4, unusual octet order BOM (2143)
-            (u'\x00\x00\xFF\xFE', {'encoding': "X-ISO-10646-UCS-4-2143", 'confidence': 1.0}),
+            (b'\x00\x00\xFF\xFE', {'encoding': "X-ISO-10646-UCS-4-2143", 'confidence': 1.0}),
             # FF FE  UTF-16, little endian BOM
-            ('\xFF\xFE', {'encoding': "UTF-16LE", 'confidence': 1.0}),
+            (b'\xFF\xFE', {'encoding': "UTF-16LE", 'confidence': 1.0}),
             # FE FF  UTF-16, big endian BOM
-            ('\xFE\xFF', {'encoding': "UTF-16BE", 'confidence': 1.0}),
+            (b'\xFE\xFF', {'encoding': "UTF-16BE", 'confidence': 1.0}),
         )
 
         aLen = len(aBuf)
